945e2d3e5ff2dd1c1decf99343cbf330767bf42f
Asif Manzoor <asifmanzoor7466@gmail.com>
2025-08-24 09:25:21 +0530

    Update switch.js

--- switch.js
+++ switch.js
@@ -1,16 +1,1 @@
-function check(){
- let result=document.getElementById("a").value
- switch(result)
- {
- case'100':
- document.getElementById("result").innerHTML="Name:Asif MAnzoor<br> Roll Number:100<br> Result:Pass";
- break;
- case'101':
- document.getElementById("result").innerHTML="Name:Fatima Farooq Bisati <br> Roll Number:101<br> Result:Pass";
- break;
- 
- 
- default:
-    document.getElementById("result").innerHTML="invalid roll number"
-}
-} 
+
